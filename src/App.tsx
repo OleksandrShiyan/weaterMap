a10296f7f66fae03955c61274b2fe4f87f37d098
Oleksandr Shiyan <52422723+OleksandrShiyan@users.noreply.github.com>
2021-07-31 11:18:23 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,7 @@ import Loader from './components/Loader/Loader';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyA1bj7mTvmSxEbupRRuH6M56BJAy1ijxMY',
-  authDomain: 'weathermap-3f4cd.firebaseapp.com',
-  projectId: 'weathermap-3f4cd',
-  storageBucket: 'weathermap-3f4cd.appspot.com',
-  messagingSenderId: '154882471955',
-  appId: '1:154882471955:web:4503edb574fc97b517e7e6',
-  measurementId: 'G-G33F7B5F0D',
+ 
 });
 
 const auth = firebase.auth();
